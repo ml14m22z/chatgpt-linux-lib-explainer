9412d9786ffb1675d1aa53cffb9f3678bcf81b19
Moke Zhang <17319370515@163.com>
2023-11-16 17:23:16 +0800

    Refactor code to improve readability and functionality.
    - Initialize `linux_lib_explainer` with `lib_name`.
    - Modify `explanation` to use `explain_function`.
    - Retrieve `function_list` from `linux_lib_explainer`.
    - Rearrange code for better flow.
    
    This commit simplifies and enhances the code by initializing `linux_lib_explainer` with `lib_name`, modifying `explanation` to use `explain_function`, and retrieving `function_list` from `linux_lib_explainer`. The code has also been rearranged for better flow and readability.

--- src/main.py
+++ src/main.py
@@ -2,17 +2,18 @@ from chatgpt_api import ChatGPTAPI
 from linux_lib_explainer import LinuxLibExplainer
 
 def main():
-    # Create an instance of ChatGPTAPI
-    chatgpt_api = ChatGPTAPI()
-
-    # Create an instance of LinuxLibExplainer
-    linux_lib_explainer = LinuxLibExplainer()
-
     # Get the name of the Linux shared library to explain
     lib_name = input("Enter the name of the Linux shared library to explain: ")
+    
+    # Create an instance of LinuxLibExplainer
+    linux_lib_explainer = LinuxLibExplainer(lib_name)
 
     # Get the explanation of the Linux shared library
-    explanation = linux_lib_explainer.explain(lib_name)
+    function_list = linux_lib_explainer.get_function_list()
+    explanation = linux_lib_explainer.explain_function(function_list[0])
+
+    # Create an instance of ChatGPTAPI
+    chatgpt_api = ChatGPTAPI()
 
     # Use ChatGPTAPI to generate a more user-friendly explanation
     user_friendly_explanation = chatgpt_api.generate_explanation(explanation)
