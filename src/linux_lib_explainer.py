2922f10ed17f693964fd81d4f849b75ccea80e39
Moke Zhang <17319370515@163.com>
2023-11-16 15:04:08 +0800

    Refactor LinuxLibExplainer to list functions available in the library
    The get_function_list() method is updated to use ctypes to fetch the list of functions available in the library.

--- src/linux_lib_explainer.py
+++ src/linux_lib_explainer.py
@@ -6,9 +6,11 @@ class LinuxLibExplainer:
         self.lib = ctypes.CDLL(self.lib_path)
 
     def get_function_list(self):
-        # This is a placeholder function. Actual implementation depends on the specific library.
-        # Typically, you would use ctypes to interact with the library and get the list of functions.
-        pass
+        function_list = []
+        for name, func in self.lib.__dict__.items():
+            if isinstance(func, ctypes._CFuncPtr):
+                function_list.append(name)
+        return function_list
 
     def explain_function(self, function_name):
         # This is a placeholder function. Actual implementation depends on the specific library.
